82ab0ced6657183f686e9c182c8ac40e8cf9b48b
Osrick Abitria <135763166+oabitria@users.noreply.github.com>
2025-04-22 12:45:12 -0400

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,11 +3,24 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const { multer, uploadToDropbox } = require('../storage'); // Import Dropbox configuration
 
+// Function to convert Dropbox shared link to direct link
+function convertToDirectLink(sharedLink) {
+    return sharedLink.replace('www.dropbox.com', 'dl.dropboxusercontent.com').replace('?dl=0', '');
+}
+
 // Post Product Route
 router.post('/post-product', multer.single('image'), async (req, res) => {
     try {
         const response = await uploadToDropbox(req.file);
-        req.body.imageUrl = response.result.path_display; // Ensure this is the correct path
+        let imageUrl = response.result.path_display; // Ensure this is the correct path
+
+        // Convert to direct link if necessary
+        if (imageUrl.includes('www.dropbox.com')) {
+            imageUrl = convertToDirectLink(imageUrl);
+        }
+
+        console.log('Dropbox image URL:', imageUrl); // Log the image URL
+        req.body.imageUrl = imageUrl;
         productController.postProduct(req, res);
     } catch (error) {
         console.error('Error uploading file:', error);
