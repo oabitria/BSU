21dedf1800ab6bbec07e28f3a81ef1cebf8e2f78
Osrick Abitria <135763166+oabitria@users.noreply.github.com>
2025-04-22 15:01:17 -0400

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,28 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const { multer, uploadToDropbox } = require('../storage'); // Import Dropbox configuration
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
-// Post Product Route
-router.post('/post-product', multer.single('image'), async (req, res) => {
-    try {
-        const response = await uploadToDropbox(req.file);
 
-        // Ensure the response is valid and contains the directLink
-        if (response && response.result && response.result.directLink) {
-            req.body.imageUrl = response.result.directLink; // Use the direct link
-            console.log('Dropbox image URL:', req.body.imageUrl); // Log the image URL
-            productController.postProduct(req, res);
-        } else {
-            throw new Error('Invalid response from Dropbox');
-        }
-    } catch (error) {
-        console.error('Error uploading file:', error);
-        res.status(500).json({ error: 'Failed to upload file', details: error.message });
+// Ensure the uploads directory exists
+const uploadDir = path.join(__dirname, '../uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname); // Ensure this line is correct
     }
 });
+
+const upload = multer({ storage: storage });
+
+
+
+
+
+// Post Product Route
+router.post('/post-product', upload.single('image'), productController.postProduct);
 
 // Fetch All Products Route
 router.get('/all', productController.getAllProducts);
 
+
 module.exports = router;
+
+//
