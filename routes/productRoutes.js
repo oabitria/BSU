5095955970e0cd048d50d144b59358fdd19300dc
Osrick Abitria <135763166+oabitria@users.noreply.github.com>
2025-04-22 11:04:09 -0400

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,39 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-
-
-// Ensure the uploads directory exists
-const uploadDir = path.join(__dirname, '../uploads');
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-}
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadDir);
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname); // Ensure this line is correct
-    }
-});
-
-const upload = multer({ storage: storage });
-
-
-
-
+const { multer, uploadToDropbox } = require('../storage'); // Import Dropbox configuration
 
 // Post Product Route
-router.post('/post-product', upload.single('image'), productController.postProduct);
+router.post('/post-product', multer.single('image'), async (req, res) => {
+    try {
+        const imageUrl = await uploadToDropbox(req.file);
+        req.body.imageUrl = imageUrl.path_display; // Store the Dropbox file path
+        productController.postProduct(req, res);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to upload file' });
+    }
+});
 
 // Fetch All Products Route
 router.get('/all', productController.getAllProducts);
 
-
 module.exports = router;
-
-//
